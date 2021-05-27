895ee262df9b7cd82ccdb552e4393cabdeb4e50e
Isadora Kondo <kondoiqcdev@gmail.com>
2021-05-27 12:11:01 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+using Figgle;
+
+namespace nomeAsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando textos em ASCII art");
+            string nome;
+            Console.Write("Digite alguma frase ou palavra: ");
+            nome=Convert.ToString(Console.ReadLine());
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine(FiggleFonts.Standard.Render(nome));
+            Console.ResetColor();
+        }
+    }
+}
